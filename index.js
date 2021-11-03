4799c74c433bc190b3f71974644b7166ea98240f
James Correa <james.correa9090@gmail.com>
2021-11-03 07:01:47 -1000

    commit tree 08060ff2029f26fa09fa480ef546a9062b33e1b1

---
+++ index.js
@@ -0,0 +1,38 @@
+function divide() {
+    return 2000 / 100
+}
+
+const square = multiply => multiply * multiply;
+
+const add = (num1, num2) => num1 + num2;
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function divide() {
+//     return 2000 / 100;
+// }
+
+// let square = multiply => multiply * multiply;
+
+// let add = (num1, num2) => num1 + num2;
